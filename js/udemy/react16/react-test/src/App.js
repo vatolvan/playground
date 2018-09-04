12a7b576105c046ea12c8a10263af5ff6569e3d7
Ville Tolvanen <ville.tolvanen@indooratlas.com>
2018-09-04 19:49:18 +0300

    test

--- js/udemy/react16/react-test/src/App.js
+++ js/udemy/react16/react-test/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
@@ -34,11 +34,12 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
     };
 
     let persons = null;
@@ -56,12 +57,22 @@ class App extends Component {
           })}
         </div>
       )
+      style.backgroundColor = 'red';
+    }
+
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+      if (this.state.persons.length <= 1) {
+        classes.push('bold');
+      }
     }
 
     return (
       <div className="App">
         <h1>Hello</h1>
-        <p>This is really working</p>
+        <p className={classes.join(' ')}>This is really working</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Switch name</button>
