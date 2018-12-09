d467ab6c5c516edfca3da9646642d7223605350b
Ville Tolvanen <ville.tolvanen@wunderdog.fi>
2018-12-09 21:37:10 +0200

    Added game manager and board manager to roguelike unity game

---
+++ unity/roguelike-demo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+    private int level = 4;
+
+	// Use this for initialization
+	void Awake() 
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame() 
+    {
+        boardScript.SetupScene(level);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
