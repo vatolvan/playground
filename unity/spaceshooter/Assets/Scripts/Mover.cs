aa62655082c0a2af82cc94beff353b7ab228a248
Ville Tolvanen <ville.tolvanen@wunderdog.fi>
2019-04-17 08:27:29 +0300

    Added spaceshooter Unity tutorial game

---
+++ unity/spaceshooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+    private void Start()
+    {
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = transform.forward * speed;
+    }
+}
---
+++ unity/spaceshooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    public float fireDelta = 0.5F;
+    private float nextFire = 0.5F;
+    private float myTime = 0.0F;
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidBody.velocity = movement * speed;
+
+        rigidBody.position = new Vector3
+        (
+            Mathf.Clamp(rigidBody.position.x, boundary.xMin, boundary.xMax), 
+            0.0f,
+            Mathf.Clamp(rigidBody.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rigidBody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidBody.velocity.x * (-tilt));
+    }
+
+    private void Update()
+    {
+        myTime += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            nextFire -= myTime;
+            myTime = 0.0F;
+        }
+    }
+}
