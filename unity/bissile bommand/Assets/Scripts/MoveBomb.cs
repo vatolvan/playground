3af0a02530ff5e9669adee945e6f9616c98d4d24
Ville Tolvanen <ville.tolvanen@indooratlas.com>
2019-04-26 21:31:38 +0300

    Added bissile command game files

---
+++ unity/bissile bommand/Assets/Scripts/MoveBomb.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBomb : MonoBehaviour
+{
+    public float speed;
+
+    public void FixedUpdate()
+    {
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+
+        rigidBody.position = rigidBody.position + new Vector3(0, -1, 0) * Time.deltaTime * speed;
+    }
+}
---
+++ unity/bissile bommand/Assets/Scripts/SpawnBombs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBombs : MonoBehaviour
+{
+    public float spawnRateSeconds;
+    public GameObject bombPrefab;
+
+    private float lastSpawn = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastSpawn > spawnRateSeconds)
+        {
+            SpawnBomb();
+            lastSpawn = Time.time;
+        }
+    }
+
+    private void SpawnBomb()
+    {
+        Debug.Log("Spawning ship");
+        Instantiate(bombPrefab, GetRandomPosition(), Quaternion.Euler(new Vector3(0, 0, 180)));
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        float r = Random.Range(-5, 5);
+        return new Vector3(r, 6, 0);
+    }
+}
